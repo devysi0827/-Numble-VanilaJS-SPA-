d85006253302d3b10e4f975e3161bfb1c04f5f5b
devysi0827 <devysi0827@gmail.com>
2023-01-16 02:48:41 +0900

    [feat] set default router

--- src/index.js
+++ src/index.js
@@ -1,4 +1,33 @@
-const router = async () => {};
+const router = async () => {
+    const routes = [
+        {path :"/", view: () => console.log('all')},
+        {path :"/image", view: () => console.log('img')},
+        {path :"/text", view: () => console.log('text')}
+    ];
+    
+    const potentialMatches = routes.map(route => {
+        return {
+            route : route,
+            isMatch : location.pathname === route.path
+        }
+        
+    })
+
+    let match = potentialMatches.find(potentialMatches => potentialMatches.isMatch);
+    
+    if (!match) {
+        match = {
+            route : routes[0],
+            isMatch : true
+        }
+    }
+    console.log(match.route.view)
+};
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    router();
+})
 
 
 
